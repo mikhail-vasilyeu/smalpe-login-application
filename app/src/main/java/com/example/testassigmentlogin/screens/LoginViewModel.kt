1c8c5cd2f6879374ca1a60b2249c5c56efb22384
Mikhail Vasilyev <mvasilyev.kate@gmail.com>
2022-06-03 08:36:25 +0200

    login and Register

--- app/src/main/java/com/example/testassigmentlogin/screens/LoginViewModel.kt
+++ app/src/main/java/com/example/testassigmentlogin/screens/LoginViewModel.kt
@@ -3,37 +3,49 @@ package com.example.testassigmentlogin.screens
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.testassigmentlogin.usecase.LoginUserUseCase
+import com.example.testassigmentlogin.usecase.RegisterUserUseCase
 import com.example.testassigmentlogin.utils.isValidEmail
 import com.example.testassigmentlogin.utils.isValidPassword
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val loginUserUseCase: LoginUserUseCase
+    private val loginUserUseCase: LoginUserUseCase,
+    private val registerUserUseCase: RegisterUserUseCase
 ) : ViewModel() {
 
-    private val _error = MutableStateFlow(Unit)
+    private val _registerSuccess = MutableSharedFlow<Unit>()
+    val registerSuccess: Flow<Unit> = _registerSuccess
+
+    private val _error = MutableSharedFlow<Unit>()
     val error: Flow<Unit> = _error
 
-    private val _navigateToApp = MutableStateFlow(Unit)
+    private val _navigateToApp = MutableSharedFlow<Unit>()
     val navigateToApp: Flow<Unit> = _navigateToApp
 
     private val _state = MutableStateFlow(LoginState())
     val state: Flow<LoginState> = _state
 
+    init {
+        Timber.d("Init")
+    }
+
     fun loginClicked(email: String, password: String) {
+        Timber.d("loginClicked: $email")
         if (validateInputs(email, password)) {
             viewModelScope.launch {
                 when (loginUserUseCase(email, password)) {
                     LoginUserUseCase.Result.Failure -> {
-                        _error.value = Unit
+                        _error.emit(Unit)
                     }
                     LoginUserUseCase.Result.Success -> {
-                        _navigateToApp.value = Unit
+                        _navigateToApp.emit(Unit)
                     }
                 }
             }
@@ -42,7 +54,12 @@ class LoginViewModel @Inject constructor(
 
     fun signUpClicked(email: String, password: String) {
         if (validateInputs(email, password)) {
-
+            viewModelScope.launch {
+                when (registerUserUseCase(email, password)) {
+                    RegisterUserUseCase.Result.Failure -> _error.emit(Unit)
+                    RegisterUserUseCase.Result.Success -> _registerSuccess.emit(Unit)
+                }
+            }
         }
     }
 
